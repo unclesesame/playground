29d010b4b8f087995ee604f024383739b1e00fff
unclesesame <guoxu_1989@163.com>
2020-03-30 21:18:26 +0800

    Interface.super.hello();

--- src/main/java/com/abner/playground/jdk/Main.java
+++ src/main/java/com/abner/playground/jdk/Main.java
@@ -3,5 +3,13 @@ package com.abner.playground.jdk;
 public class Main {
     public static void main(String[] args) {
         System.getProperties().list(System.out);
+        for(Level level: Level.values()){
+            System.out.println(level.ordinal());
+        }
+    }
+
+
+    enum Level{
+        LOW,MIDDLE,HIGH
     }
 }
