7bb09ef512c7399ad3cd9f1fefe360619bae20da
unclesesame <guoxu_1989@163.com>
2020-04-26 10:42:20 +0800

    add generic package

--- src/main/java/com/abner/playground/stream/StreamTest.java
+++ src/main/java/com/abner/playground/stream/StreamTest.java
@@ -1,13 +1,21 @@
 package com.abner.playground.stream;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 public class StreamTest {
     public static void main(String[] args) {
-        List<String> stringList = new ArrayList<>();
-        stringList.add("123");
-        stringList.add("321");
-        System.out.println(stringList.stream().count());
+/*
+ try-with-resources
+ */
+        try (Stream<String> in = Files.lines(Paths.get("StreamTest.java"));
+             PrintWriter outfile = new PrintWriter("Results.txt")) {
+            in.skip(5).limit(1).map(String::toLowerCase).forEachOrdered(outfile::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
